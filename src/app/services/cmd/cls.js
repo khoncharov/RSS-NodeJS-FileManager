7d1a798cffeb3cd4c6c58ecb2f6d6fe6573e5e48
Konstantin Honcharov <k.honcharov@gmail.com>
2022-07-09 20:44:03 +0300

    fix: delete previous version modules

--- src/app/services/cmd/cls.js
+++
@@ -1,7 +0,0 @@
-import { Cmd } from '../basic-class.js';
-
-export const clsCmd = new Cmd();
-clsCmd.argsNum = 0;
-clsCmd.executeCmd = () => {
-  console.clear();
-};
--- src/app/services/cmd/exit.js
+++
@@ -1,7 +0,0 @@
-import { Cmd } from '../basic-class.js';
-
-export const exitCmd = new Cmd();
-exitCmd.argsNum = 0;
-exitCmd.executeCmd = () => {
-  process.exit();
-};
